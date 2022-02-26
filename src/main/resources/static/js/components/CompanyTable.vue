440863ba8f4ca62c1bfd24dc98e025e21d175dac
akra <a.rahmatulin@kazinfosystems.kz>
2022-02-26 15:08:47 +0600

    Недостоющие файлы

---
+++ src/main/resources/static/js/components/CompanyTable.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-container>
+        <v-data-table
+            :headers="headers"
+            :items="companyList"
+            :items-per-page="50"
+            class="elevation-1"
+        ></v-data-table>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: {
+        companyList: {}
+    },
+    data() {
+        return {
+            headers: [
+                {
+                    text: "Ticker",
+                    value: "ticker"
+                },
+                {
+                    text: "Company Name",
+                    value: "companyName"
+                },
+                {
+                    text: "Market Cap ($ Millions)",
+                    value: "marketCap"
+                },
+                {
+                    text: "Price From",
+                    value: "priceFrom"
+                },
+                {
+                    text: "Most Recent Quater Data",
+                    value: "mostRecentQuaterData"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
